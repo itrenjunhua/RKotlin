9b94bd0fc0856b3066c8cd0e29b91119ba38c12c
Renj <itrenjunhua@163.com>
2023-03-28 17:12:04 +0800

    init project

---
+++ app/src/main/java/com/renj/kotlin/base/BaseActivity.kt
@@ -0,0 +1,19 @@
+package com.renj.kotlin.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * ======================================================================
+ *
+ * 作者：Renj
+ *
+ * 创建时间：2023-03-28   17:06
+ *
+ * 描述：
+ *
+ * 修订历史：
+ *
+ * ======================================================================
+ */
+class BaseActivity : AppCompatActivity() {
+}
